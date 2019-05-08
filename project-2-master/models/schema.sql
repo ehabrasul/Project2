30185ad3cf03715c4cdfcfb296275999a7f36859
ehabrasul <Ehab.ohio@gmail.com>
2019-05-07 20:31:35 -0400

    Update schema.sql

--- project-2-master/models/schema.sql
+++ project-2-master/models/schema.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS coderage_db;
 CREATE DATABASE coderage_db;
+
+CREATE TABLE IF NOT EXISTS tasks (
+    user_id INT AUTO_INCREMENT,
+    post VARCHAR(255) NOT NULL,
+    description TEXT,
+    PRIMARY KEY (user_id)
+) 
